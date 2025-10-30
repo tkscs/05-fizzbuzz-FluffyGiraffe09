737794167948286e4967282fb81a7e6f2f5d4bce
FluffyGiraffe09 <oroy2028@kehillahstudent.org>
2025-10-30 09:26:25 -0700

    I am divisible!

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 
+x = "I am divisible by 3 and 5!"
+y = "I am divisible by just 3!"
+z = "I am divisible by just 5!"
+
 max_number=100
 
 for i in range (1, max_number+1):
     if i % 3 == 0 and i % 5 == 0:
-        print("Fizzbuzz")
+        print(x)
     elif i % 3 == 0:
-        print('Fizz')
+        print(y)
     elif i % 5 == 0:
-        print("Buzz)")
+        print(z)
     else:
         print(i)
